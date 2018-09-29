7f78714a6ed692edfb28825d4a5a1ecc0d5acacb
Nchedo Godwin <Nchebest1@gmail.com>
2018-09-29 18:16:12 +0000

    Updated bootstrap gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplictionController
+class PagesController < ApplicationController
     def home
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    root to: 'pages#home'
+   get 'home', to: 'pages#home'
 
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
