14cdd2e8f8044c1686402cbaec40301095e457d8
Nchedo Godwin <Nchebest1@gmail.com>
2018-09-29 18:28:40 +0000

    Added a home view, controller action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-   get 'home', to: 'pages#home'
-
+ root to: 'pages#home'
 end
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
