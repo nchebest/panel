05a0100ae393c4826232d5e868834b6a36743bd3
Nchedo Godwin <Nchebest1@gmail.com>
2018-11-09 12:14:46 +0000

    Improved Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,79 @@ class PagesController < ApplicationController
     
     def about
     end
+    
+    def history
+    end
+    
+    def pastoral
+    end
+    
+    def whyAnglican
+    end
+    
+    def becomingAnglican
+    end
+    
+    def returnigToChurch
+    end
+    
+    def athletics
+    end
+   
+   def childrenMinistry
+   end
+   
+    def combineService
+    end
+    
+    def evangelism
+    end
+ 
+    def faithClinic
+    end
+    
+    def firstService
+    end
+    
+    def foundationBibleClass
+    end
+ 
+    def hospitalVisitation
+    end
+    
+    def jobs
+    end
+ 
+    def news
+    end
+ 
+    def onlinGiving
+    end
+    
+    def otherService
+    end
+    
+    def prisonVisitation
+    end
+ 
+     def schoolOfMinistry
+     end
+     
+     def secondService
+     end
+     
+     def sundayBulletin
+     end
+     
+     def teenageMinistry
+     end
+     
+     def todaysReading
+     end
+     
+     def welfare
+     end
+     
+     def youthMinistry
+     end
 end
